8381ae45cb8f74a6dbfc75068b2e0c503772d7e0
nga <ttntrinhnga@gmail.com>
2021-04-07 16:11:40 +0700

    add db

---
+++ music-backend/app/Models/Artist.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Artist extends Model
+{
+    use HasFactory;
+    protected $table = 'artist';
+}
---
+++ music-backend/database/migrations/2021_04_07_085447_create_playlists_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlaylistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('playlists', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('user');
+            $table->integer('song_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('playlists');
+    }
+}
