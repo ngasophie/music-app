a0bad81a6c4f7a31dc42713a782def100502d45a
nguyen-chi123 <57684949+nguyen-chi123@users.noreply.github.com>
2021-04-29 14:46:51 +0700

    add route search

--- music-backend/app/Http/Controllers/SongController.php
+++ music-backend/app/Http/Controllers/SongController.php
@@ -50,12 +50,43 @@ class SongController extends Controller
         ]);
     }
 
-
     public function getSongKeyword(Request $request){
         $keyword = $request->input('search'); 
-        $songs = Song::where('songTitle', 'like', '%$keyword%')->get();
-        // return response()->json(['keyword' => $keyword]);
+       
+        $name_songs = Song::where('songTitle', 'like', '%'.$keyword.'%')->get();
+        if(!empty($name_songs)){
+            foreach($name_songs as $s){
+                $s->artists = ArtistCollection::getSongArtist($s->id);
+            }
+        }
+
+        $artist_songs = DB::table('songs')
+                    ->join('song_artist', 'songs.id', '=', 'song_artist.song_id')
+                    ->join('artists', 'song_artist.artist_id', '=', 'artists.id')
+                    ->where('artists.artistName', 'like', '%'.$keyword.'%')
+                    ->distinct()
+                    ->select('songs.*')
+                    ->get();
+        foreach($artist_songs as $s){
+            $s->artists = ArtistCollection::getSongArtist($s->id);
+        }
+
+        $songs = array_merge_recursive((array)$name_songs, (array)$artist_songs);  
+        return response()->json(['songs' => $songs]);
     }
 
-    
+    public function getSongGenre($id_genre){
+        $songs = DB::table('songs') ->join('song_genre', 'songs.id', '=', 'song_genre.song_id')
+                                    ->join('genre', 'song_genre.genre_id', '=', 'genre.id')
+                                    ->select('songs.*')
+                                    ->where('genre.id', $id_genre)
+                                    ->distinct()
+                                    ->get();
+        foreach($songs as $s){
+            $s->artists = ArtistCollection::getSongArtist($s->id);
+            $s->genres = GenreCollection::getSongGenre($s->id);
+        }
+        
+        return response()->json(['songs' => $songs]);
+    }
 }
